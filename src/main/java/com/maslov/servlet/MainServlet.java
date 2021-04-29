ebe346edd95db3ec29cbb970866467d5a723cb37
yuvmaslov <yuvmaslov@ekb.beeline.ru>
2021-04-29 11:34:35 +0500

    MainServlet.java is refactored added endpoints

--- src/main/java/com/maslov/servlet/MainServlet.java
+++ src/main/java/com/maslov/servlet/MainServlet.java
@@ -24,22 +24,29 @@ public class MainServlet extends HttpServlet {
         try {
             final var path = req.getRequestURI();
             final var method = req.getMethod();
+            String endpointId = "/api/posts/\\d+";
+            String endpoint = "/api/posts";
+            String endpointSlash = "/";
             // primitive routing
-            if (method.equals("GET") && path.equals("/api/posts")) {
+            if (method.equals("GET") && path.equals(endpointSlash)) {
                 controller.all(resp);
                 return;
             }
-            if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
+            if (method.equals("GET") && path.equals(endpoint)) {
+                controller.all(resp);
+                return;
+            }
+            if (method.equals("GET") && path.matches(endpointId)) {
                 // easy way
                 final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
                 controller.getById(id, resp);
                 return;
             }
-            if (method.equals("POST") && path.equals("/api/posts")) {
+            if (method.equals("POST") && path.equals(endpoint)) {
                 controller.save(req.getReader(), resp);
                 return;
             }
-            if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
+            if (method.equals("DELETE") && path.matches(endpointId)) {
                 // easy way
                 final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
                 controller.removeById(id, resp);
